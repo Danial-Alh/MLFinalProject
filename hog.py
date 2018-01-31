46f3a42bfa01e80ff1c3d95f122a976cdaaf21d6
Danial <d.alihosseiny@gmail.com>
2018-01-31 11:12:58 +0330

    KFold for clustering implemented.

--- hog.py
+++ hog.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from numpy import uint8
 from sklearn.decomposition import PCA
-from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.model_selection import KFold
 
 from feature_extractor import load
 from models import GMM, purity_score
@@ -35,19 +35,45 @@ if __name__ == '__main__':
         hogs.append(temp)
     hogs = np.array(hogs)
     # gmm = mixture.GaussianMixture(n_components=10, verbose=True, max_iter=120)
-    gmm = GMM(n_components=10, max_iter=120)
-    hogs = hogs.reshape([60000, hogs.shape[1]])
-    counter = 1
-    while True:
-        pca = PCA(n_components=counter)
+    # gmm = GMM(n_components=10, max_iter=120)
+    # hogs = hogs.reshape([60000, hogs.shape[1]])
+    # counter = 1
+    # while True:
+    #     pca = PCA(n_components=counter)
+    #     w = pca.fit_transform(hogs)
+    #     gmm.fit(w)
+    #     l = gmm.predict(w)
+    #     train_labels = train_labels.reshape(60000)
+    #     print(counter)
+    #     print(purity_score(l, train_labels))
+    #     print(adjusted_rand_score(l, train_labels))
+    #     counter = counter + 1
+    #     print(counter)
+    #     if counter == 100:
+    #         break
+
+    kf = KFold(n_splits=3, shuffle=True)
+    puritites = []
+    pars = [10, 30, 40, 50, 60]
+    for par in pars:
+        print(par)
+        r = []
+        pca = PCA(n_components=par)
         w = pca.fit_transform(hogs)
-        gmm.fit(w)
-        l = gmm.predict(w)
-        train_labels = train_labels.reshape(60000)
-        print(counter)
-        print(purity_score(l, train_labels))
-        print(adjusted_rand_score(l, train_labels))
-        counter = counter + 1
-        print(counter)
-        if counter == 100:
-            break
+        for train_index, test_index in kf.split(w):
+            print("TRAIN:", train_index, "TEST:", test_index)
+            hog_temp_train, hog_temp_test = w[train_index], w[test_index]
+            y_temp_train, y_temp_test = train_labels[train_index], train_labels[test_index]
+            gmm = GMM(n_components=10, max_iter=120)
+            gmm.fit(hog_temp_train)
+            predicted = gmm.predict(hog_temp_test)
+            acc = purity_score(predicted, y_temp_test)
+            r.append(acc)
+            print("total purity: {}".format(acc))
+        r = np.array(r)
+        puritites.append(r.mean())
+        print("after")
+    acc_max_arg = np.argmax(puritites)
+    print("best {}: {}".format(pars[acc_max_arg], puritites[acc_max_arg]))
+    for a, c in zip(puritites, pars):
+        print("{}: {}".format(c, a))
